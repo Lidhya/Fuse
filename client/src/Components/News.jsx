1442629001da651d79523d80940c2aee38c593e9
Lidhya <lidhyajeejan@gmail.com>
2022-11-11 21:47:18 +0530

    News modal style update

--- client/src/Components/News.jsx
+++ client/src/Components/News.jsx
@@ -14,7 +14,7 @@ const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
-        width: '35rem',
+        width: 'auto',
         height: '38rem',
         backgroundColor: '#fffff',
         border: 'none'
