d591948969645474abf3515408140d20a9731f3b
Lidhya <lidhyajeejan@gmail.com>
2022-12-02 00:06:36 +0530

    then catch added in conversation controllers

--- server/controllers/conversationControllers.js
+++ server/controllers/conversationControllers.js
@@ -3,14 +3,14 @@ const Conversation = require("../models/Conversation");
 module.exports = {
     addNewConvo: async (req, res) => {
        const convo = await Conversation.findOne({members:  [req.body.senderId, req.body.receiverId] })
-       console.log(convo);
         if(convo?.members) return   res.status(200).json({exist:true});
         const newConversation = new Conversation({
             members: [req.body.senderId, req.body.receiverId],
         });
         try {
-            const savedConversation = await newConversation.save();
-            res.status(200).json(savedConversation);
+            newConversation.save()
+            .then((res)=> res.status(200).json(res))
+            .catch((error)=> res.status(500).json(error))   
         } catch (err) {
             res.status(500).json(err);
         }
@@ -18,10 +18,10 @@ module.exports = {
 
     getUserConvo: async (req, res) => {
         try {
-            const conversation = await Conversation.find({
+           Conversation.find({
                 members: { $in: [req.params.id] },
-            });
-            res.status(200).json(conversation);
+            }).then((response)=> res.status(200).json(response))
+           .catch((error)=> res.status(500).json(error))
         } catch (err) {
             res.status(500).json(err);
         }
@@ -29,10 +29,10 @@ module.exports = {
 
     getBothConvo: async (req, res) => {
         try {
-            const conversation = await Conversation.findOne({
+          Conversation.findOne({
                 members: { $all: [req.params.firstUserId, req.params.secondUserId] },
-            });
-            res.status(200).json(conversation)
+            }).then((response)=> res.status(200).json(response))
+            .catch((error)=> res.status(500).json(error))
         } catch (err) {
             res.status(500).json(err);
         }
