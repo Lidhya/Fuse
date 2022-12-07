b1b6908012a8503ae4cf6ff48cb1560bd94b0530
Lidhya <lidhyajeejan@gmail.com>
2022-12-07 08:58:10 +0530

    books root added

--- client/src/Components/LeftBar.jsx
+++ client/src/Components/LeftBar.jsx
@@ -122,7 +122,7 @@ function LeftBar() {
             </li>
             <li>
               <Link
-                to="/news"
+                to="/books"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <MenuBook />
