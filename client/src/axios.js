6f4d471c064803c5212c71703c8143a51832e858
Lidhya <lidhyajeejan@gmail.com>
2023-05-19 03:01:03 +0530

    Server url edited

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fuse.theapparel.shop/api",
 });
 
 instance.defaults.headers.post["Content-Type"] =
